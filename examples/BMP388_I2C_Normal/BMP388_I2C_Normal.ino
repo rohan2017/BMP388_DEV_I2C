bf7273e099af5d7f4712e2e8993054c83a240d4a
MartinL1 <31612615+MartinL1@users.noreply.github.com>
2020-01-09 10:08:05 +0000

    Update BMP388_I2C_Normal.ino

--- examples/BMP388_I2C_Normal/BMP388_I2C_Normal.ino
+++ examples/BMP388_I2C_Normal/BMP388_I2C_Normal.ino
@@ -11,7 +11,7 @@ void setup()
 {
   Serial.begin(115200);                             // Initialise the serial port
   bmp388.begin();                                   // Default initialisation, place the BMP388 into SLEEP_MODE 
-  bmp388.setTimeStandby(TIME_STANDBY_1280MS);       // Set the standby time to 2 seconds
+  bmp388.setTimeStandby(TIME_STANDBY_1280MS);       // Set the standby time to 1.3 seconds
   bmp388.startNormalConversion();                   // Start BMP388 continuous conversion in NORMAL_MODE 
 }
 
